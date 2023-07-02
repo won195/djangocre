e196879331c3044c8b79a95d88b721f004899485
won <71067307+won195@users.noreply.github.com>
2023-07-02 22:45:22 +0900

    Add files via upload

--- myvenv/board/urls.py
+++ myvenv/board/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'board'
 
 urlpatterns = [
-    path('', views.index),
-    path('<int:post_id>/', views.detail),
+    path('', views.index, name='index'),
+    path('<int:post_id>/', views.detail, name='detail'),
+    path('answer/create/<int:post_id>/', views.answer_create, name='answer_create'),
 ]
--- myvenv/board/views.py
+++ myvenv/board/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Post, Answer
 # Create your views here.
 
 
@@ -10,6 +11,12 @@ def index(request):
     return render(request, 'board/post_list.html', context)
 
 def detail(request, post_id):
-    post = Post.objects.get(id=post_id)
+    post = get_object_or_404(Post, pk=post_id)
     context = { 'post' : post }
     return render(request, 'board/post_detail.html', context)
+
+def answer_create(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+    answer.save()
+    return redirect('board:detail', post_id=post.id)
